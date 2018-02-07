68fb1909b0d11b61637773d1f020a16b7ecb5c45
ShatteredAtom <35435955+ShatteredAtom@users.noreply.github.com>
2018-02-06 22:08:49 -0600

    Create main.php

---
+++ sa/stg/main.php
@@ -0,0 +1,39 @@
+<?php
+namespace ServerTransgerGui;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+use pocketmine\Player;
+use muqsit\invmenu\InvMenu;
+class main {
+    /** @var InvMenu **/
+    private $menu;
+    public function __construct(string $name){
+        $menu->setName("-=ShatteredAtom Network=-");
+        $this->menu = InvMenu::create(InvMenu::TYPE_CHEST)
+            ->readonly()
+            ->setName($name)
+            ->setListener([$this, "onServerSelectorTransaction"])
+            ->onInventoryClose(function(Player $player) : void{
+                $player->sendMessage(TextFormat::GREEN."You are being transferred...");
+            });
+    }
+    public function addServerToList(Item $item, string $address, int $port) : void{
+        $nbt = $item->getNamedTag();
+        $nbt->setByte("Server", $address.":".$port);
+        $item->setNamedTagEntry($nbt->getByte("Server"));
+        $this->menu->addItem($item);
+    }
+    public function onServerSelectorTransaction(Player $player, Item $itemClickedOn) : bool{
+        $player->transfer(...explode(":", $itemClickedOn->getNamedTag()->getString("Factions", "mysticraid.tk:19132")));
+        return true;
+    }
+    public function sendTo(Player $player) : void{
+        $this->menu->send($player);
+    }
+
+$selector = new ("Server Selector");
+$selector->addServerToList(Item::get(Item::DIAMOND_SWORD), "mysticraid.tk", 19132);
+$selector->addServerToList(Item::get(Item::IRON), "nitronetwork.ddns.net", 19132);
+/** @var Player $player */
+$selector->sendTo($player);
+}
